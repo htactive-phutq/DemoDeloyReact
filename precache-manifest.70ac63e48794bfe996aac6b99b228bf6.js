b1fc822878a9c3e8ecdca5c3fa0d83583350abc8
htactive-phutq <phutq@htactive.com>
2019-11-02 16:27:29 +0700

    Updates

---
+++ precache-manifest.70ac63e48794bfe996aac6b99b228bf6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d703b98b2ab3e8de6c9f4c3ad53947cd",
+    "url": "/DemoDeloyReact/index.html"
+  },
+  {
+    "revision": "171f90c006d53e337e83",
+    "url": "/DemoDeloyReact/static/css/3.65d0fb34.chunk.css"
+  },
+  {
+    "revision": "9d26b9cf89c61094e3ef",
+    "url": "/DemoDeloyReact/static/css/main.776f1276.chunk.css"
+  },
+  {
+    "revision": "91c680f9385e8b47e1f3",
+    "url": "/DemoDeloyReact/static/js/2.f9eafe68.chunk.js"
+  },
+  {
+    "revision": "171f90c006d53e337e83",
+    "url": "/DemoDeloyReact/static/js/3.529b5dbc.chunk.js"
+  },
+  {
+    "revision": "0feea08b7535bc6da7ed",
+    "url": "/DemoDeloyReact/static/js/4.2d6454c0.chunk.js"
+  },
+  {
+    "revision": "9d26b9cf89c61094e3ef",
+    "url": "/DemoDeloyReact/static/js/main.e12e8127.chunk.js"
+  },
+  {
+    "revision": "29a1670e1b3ed2975e3d",
+    "url": "/DemoDeloyReact/static/js/runtime-main.586710ba.js"
+  },
+  {
+    "revision": "aa70f449b92c608717a6885c7871fa7b",
+    "url": "/DemoDeloyReact/static/media/1.aa70f449.jpg"
+  }
+]);
